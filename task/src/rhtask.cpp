68c634742498444aded5a3ad2604eb897578cbe0
Sergio Arroutbi Braojos <sarroutbi@marvell.com>
2021-02-07 23:34:22 +0100

    Extend functionality

--- task/src/rhtask.cpp
+++ task/src/rhtask.cpp
@@ -21,23 +21,34 @@ public:
   void dump_first() const {
     std::cout << m_buffer_list.begin()->dump() << std::endl;
   }
-  void insert_element(const buffer_t& buffer) {
-    m_buffer_list.insert(buffer);
-  }
   void dump_all() const {
     for (auto const& e : m_buffer_list) {
-      e.dump();
+      std::cout << e.dump() << std::endl;
     }
   }
   void remove_first() {
     m_buffer_list.erase(m_buffer_list.begin());
   }
+  void add(const std::string elem) {
+    uint32_t value;
+    uint32_t prio;
+    sscanf(elem.c_str(), "%u:%u", &value, &prio);
+    insert_element(buffer_t{value, prio});
+  }
 private:
+  void insert_element(const buffer_t& buffer) {
+    m_buffer_list.insert(buffer);
+  }
   std::set<buffer_t> m_buffer_list;
 };
 
 bool priority_command(const std::string& cmd) {
   // TODO: check if priority command
+  uint32_t value;
+  uint32_t prio;
+  if(sscanf(cmd.c_str(), "%u:%u", &value, &prio) != 2) {
+    return false;
+  }
   return true;
 }
 
@@ -45,16 +56,21 @@ bool supported_command(const std::string& cmd) {
   return ("get" == cmd || "print" == cmd || "remove" == cmd || priority_command(cmd));
 }
 
-void process_command(const std::string& cmd, const Buffer& b) {
+void process_command(const std::string& cmd, Buffer& b) {
   if("get" == cmd) {
     b.dump_first();
   } else if("print" == cmd) {
     b.dump_all();
+  } else if("remove" == cmd) {
+    b.remove_first();
+  } else {
+    fprintf(stderr, "ADDING:%s\n", cmd.c_str());
+    b.add(cmd);
   }
   return;
 }
 
-int analyze_command(const std::string& cmd, const Buffer& b) {
+int analyze_command(const std::string& cmd, Buffer& b) {
   if("exit" == cmd) {
     return 0;
   } else if(supported_command(cmd)) {
